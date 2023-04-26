c6e69c159b5fceec006a4f3ce1202c7e118b56eb
Krishnaraddi Basavaraddi Alagawadi <130491934+KrishnaraddiBA@users.noreply.github.com>
2023-04-26 09:34:44 +0530

    Update PostRepository.java

--- src/main/java/com/SpringFirstApplication/repository/PostRepository.java
+++ src/main/java/com/SpringFirstApplication/repository/PostRepository.java
@@ -3,7 +3,7 @@ package com.SpringFirstApplication.repository;
 import com.SpringFirstApplication.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//markerInterface
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
 }
